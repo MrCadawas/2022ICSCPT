de941a407465ff6a0997108e3f608849ea263151
FungusSir <fergus.chui@gmail.com>
2022-05-25 10:47:30 -0400

    Added another println
    yessir

--- cyberpunk99.java
+++ cyberpunk99.java
@@ -1,6 +1,7 @@
 public class cyberpunk99{
 	public static void main(String[] args){
 		System.out.println("This will be the best Cyberpunk game ever!!!");
+		System.out.println("I LOVE CYBERPUNK. I hope my pc can handle it");
 
 	}
 }
